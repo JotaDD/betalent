5f1da95a6edcaedffeaa03c0fd03c57c63d7df0d
JotaDD <joaoeduardogomes@gmail.com>
2024-07-18 16:33:16 -0300

    feat(customer): add customer migration, model and seed

---
+++ database/seeds/customer_seed.ts
@@ -0,0 +1,16 @@
+const customersSeed = [
+  {
+    name: 'Johnny Test',
+    cpf: '12345678900',
+  },
+  {
+    name: 'Susan Test',
+    cpf: '98765432100',
+  },
+  {
+    name: 'Mary Test',
+    cpf: '12345678933',
+  },
+];
+
+export default customersSeed;
