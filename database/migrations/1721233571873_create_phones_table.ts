390be45a97cbae3eaf438199a1c2742afaf9acbd
JotaDD <joaoeduardogomes@gmail.com>
2024-07-18 16:33:58 -0300

    feat(phone): add phone migration, model and seed

---
+++ database/migrations/1721233571873_create_phones_table.ts
@@ -0,0 +1,22 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'phones'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      table.string('number', 30).notNullable()
+
+      table.integer('customer_id').unsigned().references('customers.id')
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
---
+++ database/seeds/phone_seed.ts
@@ -0,0 +1,21 @@
+
+const phonesSeed = [
+  {
+    number: '5518998888888',
+    customerId: 1
+  },
+  {
+    number: '5511335369854',
+    customerId: 2
+  },
+  {
+    number: '5544995886631',
+    customerId: 1
+  },
+  {
+    number: '5518997112233',
+    customerId: 3
+  }
+]
+
+export default phonesSeed;
