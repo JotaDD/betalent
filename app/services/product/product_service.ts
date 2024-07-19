906b8532b6c83c9085d74bc7cbc5d66e268aca41
JotaDD <joaoeduardogomes@gmail.com>
2024-07-18 23:03:48 -0300

    refactor(product): add methods for get all and get all active

--- app/services/product/product_service.ts
+++ app/services/product/product_service.ts
@@ -1,14 +1,19 @@
+import CreateProductDto from "#controllers/dto/create_product_dto";
 import ProductDto from "#controllers/dto/product_dto";
 import Product from "#models/product";
 
 export default class ProductService {
 
-  async getAll(): Promise<ProductDto[]> {
-    const products = await Product.findManyBy({ isActive: true })
-
-    const sortedProducts = products.sort((a, b) => {
+  private sortAlphabeticallyByName(products: Product[]) {
+    return products.sort((a, b) => {
       return a.name.localeCompare(b.name)
     })
+  }
+
+  async getAll(): Promise<ProductDto[]> {
+    const products = await Product.all()
+
+    const sortedProducts = this.sortAlphabeticallyByName(products)
 
     const response = sortedProducts.map((product) => {
       return ProductDto.fromModel(product)
@@ -16,5 +21,19 @@ export default class ProductService {
 
     return response
   }
+
+  async getAllActive(): Promise<ProductDto[]> {
+    const products = await Product.findManyBy({ isActive: true })
+
+    const sortedProducts = this.sortAlphabeticallyByName(products)
+
+    const response = sortedProducts.map((product) => {
+      return ProductDto.fromModel(product)
+    })
+
+    return response
+  }
+
+
 
 }
