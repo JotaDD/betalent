75121de6602937964d81c5142080973f1b186f56
JotaDD <joaoeduardogomes@gmail.com>
2024-07-19 19:27:57 -0300

    feat: add phone and address interfaces

---
+++ app/interfaces/address_interface.ts
@@ -0,0 +1,10 @@
+export default interface AddressInterface {
+  id?: number
+  street: string
+  number: string
+  neighborhood: string
+  city: string
+  state: string
+  created_at?: string
+  updated_at?: string
+}
