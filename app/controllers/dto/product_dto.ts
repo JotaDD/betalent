42f142ef93eaa7f257ab12c9ccb81ce0453901c0
JotaDD <joaoeduardogomes@gmail.com>
2024-07-18 21:14:10 -0300

    feat(product): add product dto

---
+++ app/controllers/dto/product_dto.ts
@@ -0,0 +1,23 @@
+import Product from "#models/product";
+
+export default class ProductDto {
+
+  constructor(
+    public id: number,
+    public name: string,
+    public quantity: number,
+    public price: number,
+    public description: string
+  ) { }
+
+  static fromModel(product: Product): ProductDto {
+    return new ProductDto(
+      product.id,
+      product.name,
+      product.quantity,
+      product.price,
+      product.description
+    );
+  }
+  
+}
