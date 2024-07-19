0be1503f4955dcde4af9e0465617eb486bdb2507
JotaDD <joaoeduardogomes@gmail.com>
2024-07-19 19:37:21 -0300

    refactor(customer): make return a dto

--- app/services/customer/customer_service.ts
+++ app/services/customer/customer_service.ts
@@ -1,16 +1,26 @@
+import CustomerDto from "#controllers/dto/customer/customer_dto";
 import Customer from "#models/customer";
 
 export default class CustomerService {
 
-  async getAll(): Promise<Customer[]> {
+  async getAll(): Promise<CustomerDto[]> {
     const customers = await Customer.query()
-      .preload('address', builder => {
-        builder.select(['id', 'street', 'number', 'complement', 'neighborhood', 'city', 'state', 'cep'])
-      })
-      .preload('phone', builder => builder.select(['id', 'number']).orderBy('id', 'asc'))
+      .preload('address',
+        builder => builder.select([
+          'id', 'street', 'number',
+          'complement', 'neighborhood',
+          'city', 'state', 'cep'
+        ]))
+      .preload('phone',
+        (builder) => builder.select(['id', 'number'])
+          .orderBy('id', 'asc'))
       .orderBy('id', 'asc');
 
-    return customers;
+    const response = customers.map(
+      (customer) => CustomerDto.fromModel(customer)
+    )
+
+    return response;
   }
 
 
