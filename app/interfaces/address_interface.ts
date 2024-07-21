b3d3f9a4621e0d84caa717b72dea2cd4979b8067
JotaDD <joaoeduardogomes@gmail.com>
2024-07-21 13:09:31 -0300

    feat(address): add address interfaces

--- app/interfaces/address_interface.ts
+++ app/interfaces/address_interface.ts
@@ -1,10 +1,24 @@
-export default interface AddressInterface {
-  id?: number
+export interface AddressInterface {
+  id: number
   street: string
   number: string
   neighborhood: string
   city: string
   state: string
+  cep: string
   created_at?: string
   updated_at?: string
 }
+
+export interface CreateAddress {
+  street: string
+  number: string
+  neighborhood: string
+  city: string
+  state: string
+  cep: string
+}
+
+export interface AddressResponse extends AddressInterface {}
+
+export interface UpdateAddress extends CreateAddress {}
