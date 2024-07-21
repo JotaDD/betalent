e0460dcb6777e9d2adc8e6d27e0d79a305e0a0bf
JotaDD <joaoeduardogomes@gmail.com>
2024-07-21 13:20:22 -0300

    feat(customer): add customer controller with crud methods

--- app/controllers/customers_controller.ts
+++ app/controllers/customers_controller.ts
@@ -1,10 +1,11 @@
 import CustomerService from '#services/customer/customer_service'
+import { customerValidator } from '#validators/customer'
 import { inject } from '@adonisjs/core'
 import { ResponseStatus, type HttpContext } from '@adonisjs/core/http'
 
 @inject()
 export default class CustomersController {
-  constructor(private customerService: CustomerService) { }
+  constructor(private customerService: CustomerService) {}
 
   async index({ response }: HttpContext) {
     const customers = await this.customerService.getAll()
@@ -14,30 +15,47 @@ export default class CustomersController {
   /**
    * Display form to create a new record
    */
-  async create({ }: HttpContext) { }
+  async create({}: HttpContext) {}
 
   /**
    * Handle form submission for the create action
    */
-  async store({ request }: HttpContext) { }
+  async store({ request, response }: HttpContext) {
+    const data = await request.validateUsing(customerValidator)
+    const customer = await this.customerService.create(data)
+    return response.status(ResponseStatus.Created).json(customer)
+  }
+  /**
+   * Update individual record
+   */
+  async update({ params, request, response }: HttpContext) {
+    const { id } = params
+    const data = await request.validateUsing(customerValidator)
+    const customer = await this.customerService.update(id, data)
+    return response.status(ResponseStatus.Ok).json(customer)
+  }
 
   /**
    * Show individual record
    */
-  async show({ params }: HttpContext) { }
+  async show({ params, request, response }: HttpContext) {
+    const { id } = params
+    const { month, year } = request.qs()
 
-  /**
-   * Edit individual record
-   */
-  async edit({ params }: HttpContext) { }
+    const customer = await this.customerService.getById(id, month, year)
 
-  /**
-   * Handle form submission for the edit action
-   */
-  // async update({ params, request }: HttpContext) { }
+    return response.status(ResponseStatus.Ok).json(customer)
+  }
 
   /**
    * Delete record
    */
-  async destroy({ params }: HttpContext) { }
+  async delete({ params, response }: HttpContext) {
+    const { id } = params
+    await this.customerService.delete(id)
+
+    return response.status(ResponseStatus.Ok).json({
+      message: 'Customer and Orders deleted successfully',
+    })
+  }
 }
