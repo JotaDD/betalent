74cc857481992b8ab566246762c770e416388d58
JotaDD <joaoeduardogomes@gmail.com>
2024-07-21 13:06:17 -0300

    feat(product): add updateProduct validator

--- app/validators/product.ts
+++ app/validators/product.ts
@@ -1,6 +1,15 @@
 import vine from '@vinejs/vine'
 
-export const productValidator = vine.compile(
+export const updateProductValidator = vine.compile(
+  vine.object({
+    name: vine.string(),
+    quantity: vine.number(),
+    price: vine.number(),
+    description: vine.string(),
+    isActive: vine.boolean(),
+  })
+)
+export const createProductValidator = vine.compile(
   vine.object({
     name: vine.string(),
     quantity: vine.number(),
